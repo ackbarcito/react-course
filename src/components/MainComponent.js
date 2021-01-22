73757b3ac4e3bbd2294f1c2d7c5f176e2ceeb971
ack <seba_riera@hotmail.com>
2021-01-22 15:40:12 -0300

    react-redux-form Revisisted

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import { connect } from "react-redux";
 import DishDetail from "./DishdetailComponent";
 import About from "./AboutUsComponent";
 import { addComment, fetchDishes } from "../redux/ActionCreators";
+import { actions } from 'react-redux-form'
 
 const mapStateToProps = (state) => {
   return {
@@ -25,6 +26,7 @@ const mapDispatchToProps = (dispatch) => ({
   fetchDishes: () => {
     dispatch(fetchDishes());
   },
+  resetFeedbackForm: () => {dispatch(actions.reset('feedback'))}
 });
 
 class Main extends Component {
@@ -74,7 +76,7 @@ class Main extends Component {
             component={() => <Menu dishes={this.props.dishes} />}
           />
           <Route path="/menu/:dishId" component={DishWithId}></Route>
-          <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
           <Route
             exact
             path="/aboutus"
---
+++ src/redux/forms.js
@@ -0,0 +1,11 @@
+import { FormFeedback } from "reactstrap";
+
+export const InitialFeedback = {
+    firstname: '',
+    lastname: '', 
+    telnum: '',
+    email: '',
+    agree: false,
+    contactType: 'Tel.',
+    message: ''
+}
